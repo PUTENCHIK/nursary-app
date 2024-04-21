64df01a47d7062fcfdf4240149345ba19955d87c
PUTENCHIK <opaolifirenkomaxim@gmail.com>
2024-04-21 12:01:09 +0800

    Changed in users_router 10xx code errors on 404.

--- src/users/router.py
+++ src/users/router.py
@@ -27,11 +27,11 @@ def create_user(user: UserCreate, db: DBSession = Depends(get_db_session)):
     from main import special_token as app_token
 
     if get_db_user(db, login=user.login):
-        raise HTTPException(status_code=1001, detail=f"User with login '{user.login}' is already exists.")
+        raise HTTPException(status_code=404, detail=f"User with login '{user.login}' is already exists.")
         # raise UserException.user_exists(user.login)
 
     if user.is_admin and user.admin_token != app_token():
-        raise HTTPException(status_code=1002, detail=f"Entered wrong admin-token '{user.admin_token}'.")
+        raise HTTPException(status_code=404, detail=f"Entered wrong admin-token '{user.admin_token}'.")
         # raise UserException.wrong_admin_token(user.admin_token)
 
     return create_db_user(db, user)
@@ -42,7 +42,7 @@ def signin_user(user: UserPassword, db: DBSession = Depends(get_db_session)):
     user_db = get_user(user.login, db)
 
     if not signin_db_user(user, user_db):
-        raise HTTPException(status_code=1003, detail=f"Entered wrong password for login '{user.login}'.")
+        raise HTTPException(status_code=404, detail=f"Entered wrong password for login '{user.login}'.")
         # raise UserException.wrong_password(user.login)
 
     return user_db
@@ -53,7 +53,7 @@ def remove_user(user: UserPassword, db: DBSession = Depends(get_db_session)):
     user_db = get_user(user.login, db)
 
     if not signin_db_user(user, user_db):
-        raise HTTPException(status_code=1003, detail=f"Entered wrong password for login '{user.login}'.")
+        raise HTTPException(status_code=404, detail=f"Entered wrong password for login '{user.login}'.")
         # raise UserException.wrong_password(user.login)
 
     return remove_db_user(user_db)
@@ -64,7 +64,7 @@ def change_user(user: UserChange, db: DBSession = Depends(get_db_session)):
     user_db = get_user(user.login, db)
 
     if not signin_db_user(user, user_db):
-        raise HTTPException(status_code=1003, detail=f"Entered wrong password for login '{user.login}'.")
+        raise HTTPException(status_code=404, detail=f"Entered wrong password for login '{user.login}'.")
         # raise UserException.wrong_password(user.login)
 
     return change_user_fields(user_db, user)
@@ -75,7 +75,7 @@ def get_user(login: str, db: DBSession = Depends(get_db_session)):
     db_user = get_db_user(db, login=login)
 
     if db_user is None:
-        raise HTTPException(status_code=1004, detail=f"No user with login '{login}'.")
+        raise HTTPException(status_code=404, detail=f"No user with login '{login}'.")
         # raise UserException.no_user(login)
 
     return db_user
