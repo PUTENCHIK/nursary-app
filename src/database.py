28c85aee2c2f42779a4fb9e8f02e348e3f8814b6
PUTENCHIK <opaolifirenkomaxim@gmail.com>
2024-05-31 19:35:11 +0800

    Changed file paths

--- src/database.py
+++ src/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 from sqlalchemy.orm import sessionmaker
 
-db_url = "sqlite:///./src/nursary.db"
+db_url = "sqlite:///../src/nursary.db"
 
 engine = create_engine(db_url, connect_args={"check_same_thread": False})
 
