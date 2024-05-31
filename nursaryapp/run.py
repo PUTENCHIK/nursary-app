8bd068092ae1d5fa9862d8a6bf100eb749d799e3
PUTENCHIK <opaolifirenkomaxim@gmail.com>
2024-05-31 19:35:01 +0800

    Relocated main files

---
+++ nursaryapp/run.py
@@ -0,0 +1,4 @@
+import uvicorn
+from main import app
+
+uvicorn.run(app, port=5000, host="localhost")
--- run.py
+++
@@ -1,4 +0,0 @@
-import uvicorn
-from main import app
-
-uvicorn.run(app, port=5000, host="0.0.0.0")
